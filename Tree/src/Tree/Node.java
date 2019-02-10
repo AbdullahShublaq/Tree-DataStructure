0c47f43d98b5c13ca20a80b355a5a407c3cfc347
Abdullah Mohammed Shublaq <41561253+AbdullahShublaq@users.noreply.github.com>
2019-02-10 09:50:44 +0200

    Update Node.java

--- Tree/src/Tree/Node.java
+++ Tree/src/Tree/Node.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  *
- * @author Mohammed El-Agha
+ * @author Abdullah Shublaq
  */
 public class Node <E>{
     private List<Node<E>> children = new ArrayList<Node<E>>();
